beadeb4c1e17c5f7e77da0080c710b0cfe81789f
Fabricio Gatica <fabricio.gatica@ds.possumus.com.ar>
2019-01-23 20:57:33 -0300

    CRUD Marca

---
+++ Projecto/System.BackEnd/Controllers/MarcaController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Services;
+using System.Services.Models;
+
+namespace System.Backend.Controllers
+{
+    public class MarcaController : CommonController
+    {
+        private readonly ILogger<MarcaController> _logger;
+        public MarcaController(ILogger<MarcaController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            MarcaService marcaService = new MarcaService(_logger);
+            var model = marcaService.GetAll();
+            return View(model);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(MarcaViewModel marcaModel)
+        {
+            MarcaService marcaService = new MarcaService(_logger);
+            marcaService.Create(marcaModel);
+            SetTempData("Marca Creada.");
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            MarcaService marcaService = new MarcaService(_logger);
+            var model = marcaService.GetById(id);
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult Edit(MarcaViewModel modelMarca)
+        {
+            MarcaService marcaService = new MarcaService(_logger);
+            marcaService.Edit(modelMarca);
+            SetTempData("Marca Editada.");
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete (int id)
+        {
+            MarcaService marcaService = new MarcaService(_logger);
+            marcaService.Delete(id);
+            SetTempData("Marca Eliminada.");
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Projecto/System.Services/MarcaService.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Entities;
+using System.Services.Models;
+using System.Text;
+
+namespace System.Services
+{
+    public class MarcaService
+    {
+        private readonly ILogger _logger;
+        readonly UnitOfWork _uow = new UnitOfWork();
+
+        public MarcaService(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public List<MarcaViewModel> GetAll()
+        {
+            var model = _uow.MarcaRepository.All();
+            var marca = new List<MarcaViewModel>();
+            foreach (var value in model)
+            {
+                marca.Add(new MarcaViewModel { MarcaId = value.MarcaId, Nombre = value.Nombre });
+            }
+            return marca;
+        }
+
+        public MarcaViewModel GetById(int id)
+        {
+            var model = _uow.MarcaRepository.Find(x => x.MarcaId == id);
+            var marca = new MarcaViewModel()
+            {
+                MarcaId = model.MarcaId,
+                Nombre = model.Nombre
+            };
+            return marca;
+        }
+
+        public void Create (MarcaViewModel model)
+        {
+            var marca = new Marca()
+            {
+                Nombre = model.Nombre
+            };
+            _uow.MarcaRepository.Create(marca);
+            _uow.MarcaRepository.Save();
+        }
+
+        public void Edit (MarcaViewModel model)
+        {
+            var marca = _uow.MarcaRepository.Find(x => x.MarcaId == model.MarcaId);
+            marca.Nombre = model.Nombre;
+            _uow.MarcaRepository.Update(marca);
+            _uow.MarcaRepository.Save();
+        }
+
+        public void Delete (int marcaId)
+        {
+            var marca = _uow.MarcaRepository.Find(x => x.MarcaId == marcaId);
+            _uow.MarcaRepository.Delete(marca);
+            _uow.MarcaRepository.Save();
+        }
+    }
+}
