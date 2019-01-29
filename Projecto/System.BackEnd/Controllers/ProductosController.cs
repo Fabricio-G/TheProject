30eaf85b936a66c457da0a09ad1914bd986e6ab2
enzo.collado <enzo.collado@NB-PSS-010>
2019-01-28 21:16:56 -0300

    view producto

---
+++ Projecto/System.BackEnd/Controllers/ProductosController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Services;
+using System.Services.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace System.Backend.Controllers
+{
+    public class ProductosController : Controller
+    {
+        private readonly ILogger<ProductosController> _logger;
+        public ProductosController(ILogger<ProductosController> logger)
+        {
+            _logger = logger;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            ProductosService productosService = new ProductosService(_logger);
+            var model = productosService.GetAll();
+            return View(model);
+
+        }
+    }
+}
---
+++ Projecto/System.Services/ProductosService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Entities;
+using System.Services.Models;
+using System.Text;
+
+namespace System.Services
+{
+    public class ProductosService
+    {
+        private readonly ILogger _logger;   
+        readonly UnitOfWork _uow = new UnitOfWork();
+
+        public ProductosService(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public List<ProductosModel> GetAll()
+        {
+            var model = _uow.MarcaRepository.All();
+            var marca = new List<ProductosModel>();
+            foreach (var value in model)
+            {
+                marca.Add(new ProductosModel { ProductosId = value.MarcaId, Nombre = value.Nombre });
+            }
+            return marca;
+        }
+
+
+    }
+}
