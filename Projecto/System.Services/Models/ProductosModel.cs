06edc417e97e85a2f53c880b37348a924fc18db5
enzo.collado <enzo.collado@NB-PSS-010>
2019-01-28 21:16:56 -0300

    view producto

---
+++ Projecto/System.Services/Models/ProductosModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System.Services.Models
+{
+    public class ProductosModel
+    {
+        public int ProductosId { get; set; }
+        public string Nombre { get; set; }
+
+    }
+}
