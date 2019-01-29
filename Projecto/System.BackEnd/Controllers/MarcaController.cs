23de53268925513cd14a8a0a38c3bc611a66b76d
Fabricio Gatica <fabricio.gatica@ds.possumus.com.ar>
2019-01-28 21:11:59 -0300

    Try Catch y scripts

--- Projecto/System.BackEnd/Controllers/MarcaController.cs
+++ Projecto/System.BackEnd/Controllers/MarcaController.cs
@@ -18,9 +18,18 @@ namespace System.Backend.Controllers
         }
         public IActionResult Index()
         {
-            MarcaService marcaService = new MarcaService(_logger);
-            var model = marcaService.GetAll();
-            return View(model);
+            try
+            {
+                MarcaService marcaService = new MarcaService(_logger);
+                var model = marcaService.GetAll();
+                _logger.LogInformation("Listado de marcas obtenido correctamente");
+                return View(model);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError("Ocurrio un error al obtener el listado de Marcas. Error {0}", e);
+                return BadRequest("Ocurrio un error al obtener el listado de marcas");
+            }
         }
 
         public IActionResult Create()
@@ -30,33 +39,69 @@ namespace System.Backend.Controllers
         [HttpPost]
         public IActionResult Create(MarcaViewModel marcaModel)
         {
-            MarcaService marcaService = new MarcaService(_logger);
-            marcaService.Create(marcaModel);
-            SetTempData("Marca Creada.");
-            return RedirectToAction("Index");
+            try
+            {
+                MarcaService marcaService = new MarcaService(_logger);
+                marcaService.Create(marcaModel);
+                SetTempData("Marca Creada.");
+                _logger.LogInformation("Marca Creada correctamente");
+                return RedirectToAction("Index");
+            }
+            catch (Exception e)
+            {
+                _logger.LogError("Ocurrio un error al crear la marca. Error {0}", e);
+                return BadRequest("Ocurrio un error al crear la marca");
+            }
         }
 
         public IActionResult Edit(int id)
         {
-            MarcaService marcaService = new MarcaService(_logger);
-            var model = marcaService.GetById(id);
-            return View(model);
+            try
+            {
+                MarcaService marcaService = new MarcaService(_logger);
+                var model = marcaService.GetById(id);
+                _logger.LogInformation("Marca obtenida para el Id: <{0}>", id);
+                return View(model);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError("No se pudo obtener la marca para el Id: <{0}>", id);
+                return BadRequest("Ocurrio un error al obtener la marca");
+            }
         }
         [HttpPost]
         public IActionResult Edit(MarcaViewModel modelMarca)
         {
-            MarcaService marcaService = new MarcaService(_logger);
-            marcaService.Edit(modelMarca);
-            SetTempData("Marca Editada.");
-            return RedirectToAction("Index");
+            try
+            {
+                MarcaService marcaService = new MarcaService(_logger);
+                marcaService.Edit(modelMarca);
+                SetTempData("Marca Editada.");
+                _logger.LogInformation("Marca Editada Correctamente");
+                return RedirectToAction("Index");
+            }
+            catch (Exception e)
+            {
+                _logger.LogError("No se pudo editar la marca. Error <{0}>", e);
+                return BadRequest("Ocurrio un error al editar la marca");
+            }
         }
 
         public IActionResult Delete (int id)
         {
-            MarcaService marcaService = new MarcaService(_logger);
-            marcaService.Delete(id);
-            SetTempData("Marca Eliminada.");
-            return RedirectToAction("Index");
+            try
+            {
+                MarcaService marcaService = new MarcaService(_logger);
+                marcaService.Delete(id);
+                SetTempData("Marca Eliminada.");
+                _logger.LogInformation("Marca eliminada correctamente");
+                return RedirectToAction("Index");
+            }
+            catch (Exception e)
+            {
+                _logger.LogError("No se pudo eliminar la marca para el Id: <{0}>. Error {1}", id, e);
+                return BadRequest("Ocurrio un error al eliminar la marca");
+            }
         }
     }
 }
