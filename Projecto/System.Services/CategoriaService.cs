77523bda35dab34c7bbab031fda1a1307fa862f8
Fabricio Gatica <fabricio.gatica@ds.possumus.com.ar>
2019-01-31 19:22:59 -0300

    Add Servicio,Modelo,Vistas,Controller

---
+++ Projecto/System.Services/CategoriaService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Entities;
+using System.Services.Models;
+using System.Text;
+
+namespace System.Services
+{
+    public class CategoriaService
+    {
+        private readonly ILogger _logger;
+        readonly UnitOfWork _uow = new UnitOfWork();
+
+        public CategoriaService(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public List<CategoriaViewModel> GetAll()
+        {
+            var model = _uow.CategoriaRepository.All();
+            var categoria = new List<CategoriaViewModel>();
+            foreach (var cat in model)
+            {
+                categoria.Add(new CategoriaViewModel { CategoriaId = cat.CategoriaId, Nombre = cat.Nombre });
+            }
+            return categoria;
+        }
+    }
+}
---
+++ Projecto/System.Services/Models/CategoriaViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System.Services.Models
+{
+    public class CategoriaViewModel
+    {
+        public int CategoriaId { get; set; }
+        public string Nombre { get; set; }
+    }
+}
