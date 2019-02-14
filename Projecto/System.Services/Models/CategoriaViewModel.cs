4e067db5035a00708da21546c78b59eb79ad107b
Fabricio Gatica <fabricio.gatica@ds.possumus.com.ar>
2019-02-13 22:24:41 -0300

    Validaciones modelos

--- Projecto/System.Services/Models/CategoriaViewModel.cs
+++ Projecto/System.Services/Models/CategoriaViewModel.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace System.Services.Models
 {
     public class CategoriaViewModel
     {
+        [Required(ErrorMessage = "El campo Id es obligatorio")]
         public int CategoriaId { get; set; }
+        [Required(ErrorMessage = "El campo Nombre es obligatorio")]
         public string Nombre { get; set; }
     }
 }
