62f40aed85013b22b7444e951bf362a893504ea2
enzo.collado <enzo.collado@NB-PSS-010>
2019-03-07 16:11:52 -0300

    Delete de migraciones que no sirven, agregar seeder que falta, elimininacion de modelo de producto de mas

--- Projecto/System.Entities/Migrations/20190131212634_UpdateTableProducto.cs
+++ Projecto/System.Entities/Migrations/20190131212634_UpdateTableProducto.cs
@@ -1,25 +1,25 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
-using System;
-using System.Collections.Generic;
+﻿//using Microsoft.EntityFrameworkCore.Migrations;
+//using System;
+//using System.Collections.Generic;
 
-namespace System.Entities.Migrations
-{
-    public partial class UpdateTableProducto : Migration
-    {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.RenameColumn(
-                name: "Imagen",
-                table: "Producto",
-                newName: "Descripcion");
-        }
+//namespace System.Entities.Migrations
+//{
+//    public partial class UpdateTableProducto : Migration
+//    {
+//        protected override void Up(MigrationBuilder migrationBuilder)
+//        {
+//            migrationBuilder.RenameColumn(
+//                name: "Imagen",
+//                table: "Producto",
+//                newName: "Descripcion");
+//        }
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.RenameColumn(
-                name: "Descripcion",
-                table: "Producto",
-                newName: "Imagen");
-        }
-    }
-}
+//        protected override void Down(MigrationBuilder migrationBuilder)
+//        {
+//            migrationBuilder.RenameColumn(
+//                name: "Descripcion",
+//                table: "Producto",
+//                newName: "Imagen");
+//        }
+//    }
+//}
---
+++ Projecto/System.Entities/Migrations/20190307170129_SeederVista.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace System.Entities.Migrations
+{
+    public partial class SeederVista : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+            table: "Vista",
+            columns: new[] { "VistaId", "Descripcion", "Nombre"},
+            values: new object[] { 1, "Perfil", "Gestión de perfiles"});
+
+            migrationBuilder.InsertData(
+            table: "Vista",
+            columns: new[] { "VistaId", "Descripcion", "Nombre" },
+            values: new object[] { 2, "Usuario", "Gestión de usuarios" });
+
+            migrationBuilder.InsertData(
+            table: "Vista",
+            columns: new[] { "VistaId", "Descripcion", "Nombre" },
+            values: new object[] { 3, "Parametro", "Gestión de parámetros" });
+
+            migrationBuilder.InsertData(
+            table: "Vista",
+            columns: new[] { "VistaId", "Descripcion", "Nombre" },
+            values: new object[] { 4, "Auditoria", "Gestión de audotría" });
+
+            migrationBuilder.InsertData(
+            table: "Vista",
+            columns: new[] { "VistaId", "Descripcion", "Nombre" },
+            values: new object[] {5, "Marca", "Gestion de marcas" });
+
+            migrationBuilder.InsertData(
+            table: "Vista",
+            columns: new[] { "VistaId", "Descripcion", "Nombre" },
+            values: new object[] { 6, "Categoria", "Gestion de categorias" });
+
+            migrationBuilder.InsertData(
+            table: "Vista",
+            columns: new[] { "VistaId", "Descripcion", "Nombre" },
+            values: new object[] { 7, "Producto", "Gestion de productos" });
+
+
+            migrationBuilder.InsertData(
+            table: "Vista",
+            columns: new[] { "VistaId", "Descripcion", "Nombre" },
+            values: new object[] { 8, "Empleados", "Gestion de empleados" });
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
