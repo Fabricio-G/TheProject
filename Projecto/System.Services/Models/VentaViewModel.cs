adbc6933a1f907d996eefe46b0f39efe4b9fa876
DamianFCode <fabricio.gatica@ds.possumus.com.ar>
2019-05-01 20:51:36 -0300

    Ventas

---
+++ Projecto/System.Services/Models/VentaViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System.Services.Models
+{
+    public class VentaViewModel
+    {
+        public string Monto { get; set; }
+        public int CategoriaId { get; set; }
+        public int MarcaId { get; set; }
+        public int ProductoId { get; set; }
+    }
+}
